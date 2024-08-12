a8cbe8292b43717fd26a42df284e36ffb67726de
lorenagrasu <lorenagrasu.13@gmail.com>
2024-08-12 16:47:08 +0300

    Implement PlaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -44,22 +44,23 @@ const AuthForm = ({type}: {type: string}) => {
         setIsLoading(true);
         
         try {
+        
             if(type === 'sign-up'){
-                // const userData = {
-                //     firstName: data.firstName!,
-                //     lastName: data.lastName!,
-                //     address1: data.address1!,
-                //     city: data.city!,
-                //     state: data.state!,
-                //     postalCode: data.postalCode!,
-                //     dateOfBirth: data.dateOfBirth!,
-                //     ssn: data.ssn!,
-                //     email: data.email,
-                //     password: data.password
-                //   }
-                 const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                }
+                const newUser = await signUp(userData);
 
-                 setUser(newUser)
+                setUser(newUser)
             }
             if(type === 'sign-in'){
                  const response = await signIn({
@@ -107,7 +108,7 @@ const AuthForm = ({type}: {type: string}) => {
         </header>
         {user ? (
             <div className="flex flex-col gap-4">
-                <PlaidLink />
+                <PlaidLink user={user} variant="primary"/>
             </div>
         ): (
             <>
